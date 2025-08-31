67cea22bc41dfadf1796e76ce1cba0c1ca8351b6
AmritHehe <hopesalive1506@gmail.com>
2025-08-31 19:16:02 +0530

    v0 done

--- src/index.ts
+++ src/index.ts
@@ -4,56 +4,120 @@ import jwt, { type JwtHeader, type JwtPayload } from "jsonwebtoken"
 import { JWT_PASSWORD } from "./configure.js"
 import { useMiddleware } from "./middleware.js"
 import { createClient } from "redis"
+import cors from "cors"
 const app = express()
 
 
+app.use(cors())
 const sub = createClient(); 
 await sub.connect(); 
 let  priceBTC :number
-
-await sub.subscribe('trades' , (message) => { priceBTC = JSON.parse(message).price
-    
-    ;
-    // console.log(priceBTC)
-
-    //liquidisation ! will come here 
-})
-function closeOrder(username : string , orderId : number){ 
-    let user = users.get(username); 
-    if(user){
-    const order = user?.wallet.find(O => O.orderId === orderId )
-    order!.orderType="closed"
-    users.set(username , user)
-    }
-
-}
-sub.on("message" , (username)=> { 
-    let user =  users.get(username); 
-    let wallet = user?.wallet; 
-    if(wallet){
-        for(let i = 0 ; i < wallet!.length ; i++){ 
-            if(wallet[i]!.orderType =="open" ){ 
-                if(!wallet[i]?.leverage){
-                    let currentprice = priceBTC * wallet[i]!.quantity; 
-                    let loss = wallet[i]!.buyPrice - currentprice;
-                    if(loss >= 0.9*wallet[i]!.buyPrice){ 
-                     closeOrder(username , wallet[i]!.orderId)
+let  buyPriceBTC : number
+await sub.subscribe('trades' , (message) => { priceBTC = JSON.parse(message).price;
+    buyPriceBTC = JSON.parse(message).buy;
+     for ( const [username , user] of users){
+        let wallet = user.wallet; 
+        if(wallet){
+            for(let j = 0 ; j < wallet!.length ; j++){ 
+                if(wallet[j]!.orderType =="open" ){ 
+                    if(!wallet[j]?.leverage){
+                        let currentprice = priceBTC * wallet[j]!.quantity; 
+                        let buyCurrentPrice = buyPriceBTC * wallet[j]!.quantity 
+                        let loss ;
+                        if(wallet[j]?.tradeType=="buy"){ 
+                            loss =  wallet[j]!.buyPrice - currentprice;
+                            console.log("loss " + loss )
+                            console.log("bought at price " + wallet[j]!.buyPrice + "currentbtc price " + priceBTC)
+                        } 
+                        else if (wallet[j]?.tradeType == "sell"){ 
+                            loss = buyCurrentPrice - wallet[j]!.buyPrice;
+                            console.log("loss " + loss )
+                            console.log("bought at price " + wallet[j]!.buyPrice + "currentbtc price " + buyPriceBTC)
+                        }
+                        console.log()
+                        if(loss){ 
+                            if(loss >= 0.9*wallet[j]!.buyPrice){ 
+                                closeOrder(username , wallet[j]!.orderId , priceBTC , currentprice)
+                                console.log("sold the order hehe")
+                            }
+                        }
                     }
-                }
-                else if(wallet[i]?.leverage){ 
-                    let currentprice = priceBTC * wallet[i]!.quantity * wallet[i]!.l;
-                    let buyLeveragedPrice = wallet[i]?.buyLeveragedPrice ; 
-                    if(buyLeveragedPrice){
-                        let loss = buyLeveragedPrice - currentprice 
-                        if(loss >= 0.9*wallet[i]!.buyPrice){ 
-                            closeOrder(username, wallet[i]!.orderId)
+                    else if(wallet[j]?.leverage){ 
+                        let currentprice = priceBTC * wallet[j]!.quantity ;
+                        let buyCurrentPrice = buyPriceBTC*wallet[j]!.quantity;
+                        let buyLeveragedPrice = wallet[j]?.buyLeveragedPrice ; 
+                        let entryprice = wallet[j]!.buyPrice
+                        if(buyLeveragedPrice){
+                            let loss; 
+                            if(wallet[j]?.tradeType == "buy"){
+                                loss = (entryprice - currentprice) * wallet[j]!.l
+                                console.log("loss " + loss )
+                                console.log("bought at price " + buyLeveragedPrice + "currentbtc price " + priceBTC)
+                            }
+                            else if(wallet[j]?.tradeType =="sell"){ 
+                                loss = (buyCurrentPrice - entryprice) *wallet[j]!.l
+                                console.log("loss " + loss )
+                                console.log("bought at price " + buyLeveragedPrice + "currentbtc price " + buyPriceBTC)
+                            }
+                            if(loss){
+                                if(loss >= 0.9*entryprice){ 
+                                    closeOrder(username, wallet[j]!.orderId , priceBTC , currentprice ) 
+                                    console.log("sold the order  ")
+                                }
+                            }
                         }
                     }
                 }
             }
         }
     }
+    ;
+    // console.log(priceBTC)
+
+    //liquidisation ! will come here 
 })
+function closeOrder(username : string , orderId : number , priceBTC : number , sellingleveragedPrice : number){ 
+    let user = users.get(username); 
+    if(user){
+    const order = user?.wallet.find(O => O.orderId === orderId )
+    order!.orderType="closed" ; 
+    order!.sellPrice = priceBTC ; 
+    order!.soldLeveragedPrice =  sellingleveragedPrice;
+    users.set(username , user)
+    }
+
+}
+// sub.on("message" , ()=> { 
+//     for ( const [username , user] of users){
+//         let wallet = user.wallet; 
+//         if(wallet){
+//             for(let j = 0 ; j < wallet!.length ; j++){ 
+//                 if(wallet[j]!.orderType =="open" ){ 
+//                     if(!wallet[j]?.leverage){
+//                         let currentprice = priceBTC * wallet[j]!.quantity; 
+//                         let loss = wallet[j]!.buyPrice - currentprice;
+//                         if(loss >= 0.9*wallet[j]!.buyPrice){ 
+//                             closeOrder(username , wallet[j]!.orderId)
+//                             console.log("sold the order hehe")
+//                         }
+//                     }
+//                     else if(wallet[j]?.leverage){ 
+//                         let currentprice = priceBTC * wallet[j]!.quantity * wallet[j]!.l;
+//                         let buyLeveragedPrice = wallet[j]?.buyLeveragedPrice ; 
+//                         if(buyLeveragedPrice){
+//                             let loss = buyLeveragedPrice - currentprice 
+//                             console.log("loss " + loss )
+//                             if(loss >= 0.9*wallet[j]!.buyPrice){ 
+//                                 closeOrder(username, wallet[j]!.orderId)
+//                                 console.log("sold the order ")
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// })
 
 
 const client = new Client({
@@ -75,6 +139,7 @@ let users =  new Map<string,{
     orderId : number ;
     coin : string ;
     quantity : number ;
+    tradeType ?: string;
     type ?: string ;
     leverage : boolean ;
     l : number ;
@@ -153,7 +218,7 @@ app.get("/candles/1m" , async (req, res)=> {
     try { 
         const result  = await client.query(
             `SELECT * FROM candle_1m
-             ORDER BY bucket DESC LIMIT 20` 
+             ORDER BY bucket DESC LIMIT 1000` 
         );
         console.log("query sucess")
         return res.json(result.rows);
@@ -179,9 +244,9 @@ app.get("/balance" , useMiddleware,(req , res) => {
     }
 
 })
-
 app.post("/order/open", useMiddleware , async(req , res)=> { 
-    const currentprice = priceBTC;
+    const currentprice = buyPriceBTC;
+    const currentSellPrice = priceBTC;
     const token  :any = req.headers.authorization;
     const payload = req.body ;
     const orderid = Date.now()
@@ -201,6 +266,7 @@ app.post("/order/open", useMiddleware , async(req , res)=> {
         let balance = user.balance; 
         const qty = payload.qty; 
         let totalprice = currentprice * qty;
+        let totalSellPrice = currentSellPrice *qty
         if(payload.type == "buy" ){
             console.log("yha tk bhi agya")
             if(balance >=totalprice){
@@ -209,7 +275,7 @@ app.post("/order/open", useMiddleware , async(req , res)=> {
                     //@ts-ignore
                     user.balance = balance; 
                     //@ts-ignore
-                    user.wallet.push({coin : payload.asset  , quantity : qty , type : "buy" , leverage :false , l :1 , orderId : orderid , orderType : "open" , buyPrice : totalprice})
+                    user.wallet.push({coin : payload.asset  , quantity : qty ,tradeType:"buy", type : "buy" , leverage :false , l :1 , orderId : orderid , orderType : "open" , buyPrice : totalprice})
                     users.set(username, user);
                 }
 
@@ -220,7 +286,7 @@ app.post("/order/open", useMiddleware , async(req , res)=> {
                     user.balance = balance; 
                      const leveragedPrice = currentprice*qty*leverage;
                      //@ts-ignore
-                     user.wallet.push({coin : payload.asset  , quantity : qty , type : "buy" , leverage : true ,l : leverage , buyLeveragedPrice :leveragedPrice , orderId : orderid , orderType : "open" , buyPrice : totalprice})
+                     user.wallet.push({coin : payload.asset  , quantity : qty , tradeType : "buy" , type : "buy" , leverage : true ,l : leverage , buyLeveragedPrice :leveragedPrice , orderId : orderid , orderType : "open" , buyPrice : totalprice})
                      users.set(username, user);
                     }
 
@@ -229,7 +295,8 @@ app.post("/order/open", useMiddleware , async(req , res)=> {
                 }
             }
             else{ 
-                return res.status(403).json("you dont have enough balance")
+                return res.status(403).json("you dont have enough balance"+balance+"total price" + totalprice
+                ) 
             }
         }
         else if(payload.type == "sell"){ 
@@ -237,7 +304,8 @@ app.post("/order/open", useMiddleware , async(req , res)=> {
             const order = user.wallet.find(o => o.orderId === orderid)
             console.log("found the order" + order)
             if(!order?.leverage){
-                balance += totalprice;
+                let profit = totalSellPrice - order!.buyPrice;
+                balance = balance + (order!.buyPrice + profit);
                 //@ts-ignore
                 user.balance = balance; 
                 
@@ -250,9 +318,9 @@ app.post("/order/open", useMiddleware , async(req , res)=> {
                let buyleveragedPrice =  order.buyLeveragedPrice 
                if(buyleveragedPrice){
                let leverage = order.l
-               let sellingleveragedPrice = currentprice*qty*leverage;
-               let currentvalue = sellingleveragedPrice - buyleveragedPrice;
-               balance = balance + currentvalue + payload.buyPrice;
+               let sellingleveragedPrice = currentSellPrice*qty*leverage;
+               let profit = sellingleveragedPrice - buyleveragedPrice;
+               balance = balance + (buyPrice + profit)
                order.soldLeveragedPrice = sellingleveragedPrice;
                user.balance = balance ; 
                order!.orderType = "closed"
@@ -296,21 +364,137 @@ app.post("/order/open", useMiddleware , async(req , res)=> {
     // //tade profit
     
 })
-app.get("/order" , async(req ,res)=> { 
+app.post("/trade/sell"  , async(req , res)=> { 
+    const currentprice = priceBTC;
+    const currentSellPrice = buyPriceBTC;
+    const token  :any = req.headers.authorization;
+    const payload = req.body ;
+    const orderid = Date.now()
+    console.log("payload" + JSON.stringify(payload))
+    //payload  { 
+    // qty 
+    // type 
+    // asset 
+    // leverage } 
+    if(token){
+    const userDetails :any  = jwt.verify(token, JWT_PASSWORD) ;
+    const username  = userDetails.username ;
+    let user = users.get(username)
+    
+    if(user){
+        console.log("user to milgya")
+        let balance = user.balance; 
+        const qty = payload.qty; 
+        let totalprice = currentprice * qty;
+        let totalSellPrice = currentSellPrice *qty
+        if(payload.type == "buy" ){
+            console.log("yha tk bhi agya")
+            if(balance >=totalprice){
+                if(!payload.leverage){
+                    balance -= totalprice ;
+                    //@ts-ignore
+                    user.balance = balance; 
+                    //@ts-ignore
+                    user.wallet.push({coin : payload.asset  , quantity : qty , tradeType : "sell" , type : "buy" , leverage :false , l :1 , orderId : orderid , orderType : "open" , buyPrice : totalprice})
+                    users.set(username, user);
+                }
 
+                else if(payload.leverage) { 
+                    const leverage = payload.l;
+                    balance -= totalprice;
+                    //@ts-ignore
+                    user.balance = balance; 
+                     const leveragedPrice = currentprice*qty*leverage;
+                     //@ts-ignore
+                     user.wallet.push({coin : payload.asset  , quantity : qty , tradeType:"sell" , type : "buy" , leverage : true ,l : leverage , buyLeveragedPrice :leveragedPrice , orderId : orderid , orderType : "open" , buyPrice : totalprice})
+                     users.set(username, user);
+                    }
+
+                else { 
+                    return res.json("good dev if you are here")
+                }
+            }
+            else{ 
+                return res.status(403).json("you dont have enough balance"+balance+"total price" + totalprice
+                ) 
+            }
+        }
+        else if(payload.type == "sell"){ 
+            const orderid : number= payload.orderId
+            const order = user.wallet.find(o => o.orderId === orderid)
+            console.log("found the order" + order)
+            if(!order?.leverage){
+                let profit = order!.buyPrice - totalSellPrice;
+                balance = balance + (order!.buyPrice + profit);
+                //@ts-ignore
+                user.balance = balance; 
+                
+                order!.orderType = "closed"
+                order!.sellPrice = totalprice;
+                users.set(username, user);
+            }
+            else if (order?.leverage){ 
+               let buyPrice = order.buyPrice ;
+               let buyleveragedPrice =  order.buyLeveragedPrice 
+               if(buyleveragedPrice){
+               let leverage = order.l
+               let sellingleveragedPrice = currentSellPrice*qty*leverage;
+               console.log("selling leverage price" + sellingleveragedPrice)
+               let profit = buyleveragedPrice - sellingleveragedPrice ;
+               balance = balance + (buyPrice+ profit) ;
+               order.soldLeveragedPrice = sellingleveragedPrice;
+               user.balance = balance ; 
+               order!.orderType = "closed"
+               order.sellPrice = totalprice;
+
+               users.set(username, user);
+
+               }
+               else { 
+                console.log("some error happened")
+               }
+            }
+        }
+        
+        
+        //@ts-ignore
+        
+        console.log(users)
+        res.json("trade executed successfully  balance remaining " + user.balance + " wallet " + JSON.stringify(user.wallet) + "order total price" + totalprice )
+        }
+
+    } 
+
+})
+app.get("/order/open" , async(req ,res)=> { 
+    const token :string | undefined = req.headers.authorization ;
+    if(token){
+         const userDetails :any  = jwt.verify(token, JWT_PASSWORD) ;
+         const username   = userDetails.username ;
+         const user = users.get(username)
+         const openOrders = user?.wallet.filter( o => o.orderType == "open")
+        res.json({"open orders" : openOrders})
+    }
+    else{ 
+        alert("log in first")
+        return;
+    }
     
     
 }) 
-app.post("/order/close" , async (req, res)=> { 
+app.get("/order/close" , async (req, res)=> { 
     //close the order 
-    const asset = req.body.asset;
-    let balance = req.body.balance ; 
-    const quantity = req.body.quantity ;
-    const currentprice :number =  parseFloat(JSON.stringify(await client.query(`SELECT price FROM trades_raw WHERE asset = '${asset}' ORDER BY time DESC LIMIT 1`)))
-    balance += currentprice * quantity
-
-    res.json({ 
-        balance : balance
-    })
+    const token :string | undefined = req.headers.authorization ;
+    if(token){
+         const userDetails :any  = jwt.verify(token, JWT_PASSWORD) ;
+         const username   = userDetails.username ;
+         const user = users.get(username)
+         const openOrders =  user?.wallet.filter(o => o.orderType == "closed")
+        res.json({"open orders" : openOrders})
+    }
+    else{ 
+        alert("log in first")
+        return;
+    }
 })
 app.listen(3000)
